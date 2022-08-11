38b24280770251d1451045f45282bb67eba4ed33
Israel AyoOluwa Adeleke <izzyfresher@gmail.com>
2022-08-11 11:33:42 +0100

    Edited Readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,34 +11,38 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
-
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
   const { data, isLoading } = getAllAsset();
 
-  if(isLoading) {
-    return <Loader/>
+  if (isLoading) {
+    return <Loader />;
   }
   return (
     <div className="font-primary bg-primary text-black">
       <Head>
-        <title>Create Next App</title>
+        <title>ASAlytics</title>
         <link rel="icon" href="/assets/favicon.svg" />
+        <meta name="description" content="ASAltyics Test" />
+        <meta name="author" content="Israel AyoOluwa" />
       </Head>
 
       <Header />
 
-      <h1 className="md:text-[36px] font-medium md:w-[45%] w-[80%] mx-auto text-center my-[2em] text-[22px]" data-aos="fade-in" 
-     data-aos-duration="1500">
+      <h1
+        className="md:text-[36px] font-medium md:w-[45%] w-[80%] mx-auto text-center my-[2em] text-[22px]"
+        data-aos="fade-in"
+        data-aos-duration="1500"
+      >
         List of Algorand Standard Assets on ASAlytics
       </h1>
 
       <div className="w-[85%] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[1em]">
-        {data?.results.map((assets: any[]) =>
-           <Card assets={assets} />
-        )}
+        {data?.results.map((assets: any[]) => (
+          <Card assets={assets} />
+        ))}
       </div>
     </div>
   );
